00aa6745cc48e060d8e8fa5b8f1c51c79a5a6674
Alexis Linard <al.linard@gmail.com>
2014-03-10 21:34:48 +0100

    Ecran de garde

--- gen/com/example/locateunivnantes/R.java
+++ gen/com/example/locateunivnantes/R.java
@@ -22,9 +22,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logounivnantes=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int btnAide=0x7f080003;
+        public static final int btnEntrer=0x7f080001;
+        public static final int btnQuitter=0x7f080002;
+        public static final int imageView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/locateunivnantes/MainActivity.java
+++ src/com/example/locateunivnantes/MainActivity.java
@@ -3,13 +3,25 @@ package com.example.locateunivnantes;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
+	Button buttonEntrer;
+	Button buttonQuitter;
+	Button buttonAide;
+
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		addListenerOnButtonEntrer();
+		addListenerOnButtonQuitter();
+		addListenerOnButtonAide();
     }
 
 
@@ -20,4 +32,41 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    
+	public void addListenerOnButtonEntrer() {
+		buttonEntrer = (Button) findViewById(R.id.btnEntrer);
+		buttonEntrer.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				//TODO ce qui se passe qd on clique
+			}
+ 
+		});
+	}
+	
+	
+	public void addListenerOnButtonQuitter() {
+		buttonQuitter = (Button) findViewById(R.id.btnQuitter);
+		buttonQuitter.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				finish();
+				System.exit(0);
+			}
+ 
+		});
+	}
+	
+	
+	public void addListenerOnButtonAide() {
+		buttonAide = (Button) findViewById(R.id.btnAide);
+		buttonAide.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				//TODO ce qui se passe qd on clique
+			}
+ 
+		});
+	}
+	
 }
