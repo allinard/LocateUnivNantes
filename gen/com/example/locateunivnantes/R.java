6031eebf2ff1d2a6bdd7c56b8639e25b32ccc65a
Adeline Gr <adeline.granet@gmail.com>
2014-03-31 13:42:22 +0200

    icon logo

--- gen/com/example/locateunivnantes/R.java
+++ gen/com/example/locateunivnantes/R.java
@@ -38,8 +38,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int logounivnantes=0x7f020002;
+        public static final int ic_logo_locunivnantes=0x7f020001;
+        public static final int ic_logo_locunivnantes2=0x7f020002;
+        public static final int icon=0x7f020003;
+        public static final int logounivnantes=0x7f020004;
     }
     public static final class id {
         /**  Messages IDs 
